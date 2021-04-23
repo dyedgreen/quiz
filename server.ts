1e08b8974205da3b71246f5b95121435b206fb2d
Tilman Roeder <dyed.green.info@gmail.com>
2021-04-23 16:44:56 +0100

    Try to catch all errors?

--- server.ts
+++ server.ts
@@ -2,18 +2,29 @@ import { serve } from "https://deno.land/std@0.93.0/http/server.ts";
 import serveFiles from "./lib/files.ts";
 import { serveApi } from "./lib/api.ts";
 
-const server = serve({ port: 8080 });
-console.log("Server Started ...");
+async function run() {
+  const server = serve({ port: 8080 });
+  console.log("Server Started ...");
 
-for await (const req of server) {
-  console.log(`[${new Date()}] ${req.method} ${req.url}`);
-  try {
-    if (/^\/api\/.+/.test(req.url)) {
-      serveApi(req);
-    } else {
-      serveFiles(req, "www").then(resp => req.respond(resp));
+  for await (const req of server) {
+    console.log(`[${new Date()}] ${req.method} ${req.url}`);
+    try {
+      if (/^\/api\/.+/.test(req.url)) {
+        serveApi(req);
+      } else {
+        serveFiles(req, "www").then(resp => req.respond(resp));
+      }
+    } catch (err) {
+      console.error(`[${new Date()}] Request error: ${err}`);
     }
-  } catch (err) {
-    console.error(`[${new Date()}] Request error: ${err}`);
   }
 }
+
+function retry() {
+  run().catch(err => {
+    console.error(`[${new Date()}] Overall error: ${err}`);
+    retry();
+  });
+}
+
+retry();
